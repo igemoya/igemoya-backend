8d752ddc81b9720eb5e4c135ae8be1d11091a3e3
electrochemistry04 <electrochemistry04@outlook.com>
2021-08-23 22:18:41 +0900

    exhibition model

--- src/models/exhibition.ts
+++ src/models/exhibition.ts
@@ -1,35 +1,49 @@
-import { Schema, model } from 'mongoose';
-import { ObjectId } from 'mongodb';
-import { exhibitionGeoTypeValues } from '../types';
+import { Schema, model } from "mongoose";
+import { ObjectId } from "mongodb";
+import { exhibitionGeoTypeValues } from "../types";
+import {
+  ObjectIdentity,
+  ItemIdentity,
+  ExhibitionIdentity,
+} from "../interfaces";
 
-const objectSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  images: { type: Array, required: true },
-}, { timestamps: true });
+const objectSchema = new Schema<ObjectIdentity>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    images: { type: Array, required: true },
+  },
+  { timestamps: true }
+);
 
-const itemSchema = new Schema({
+const itemSchema = new Schema<ItemIdentity>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    images: { type: Array, required: true },
+    location: {
+      coordinate: { type: Array, required: true },
+      type: { type: String, enum: exhibitionGeoTypeValues },
+    },
+    objects: [objectSchema],
+  },
+  { timestamps: true }
+);
+
+const exhibitionSchema = new Schema<ExhibitionIdentity>({
+  createdUser: { type: ObjectId, ref: "User" },
   name: { type: String, required: true },
-  description: { type: String, required: true },
   images: { type: Array, required: true },
   location: {
     coordinate: { type: Array, required: true },
-    type: { type: String, enum: exhibitionGeoTypeValues }
+    type: { type: String, enum: exhibitionGeoTypeValues },
   },
-  objects: [objectSchema]
-}, { timestamps: true });
-
-const exhibitionSchema = new Schema({
-  createdUser: { type: ObjectId, ref: 'User' },
-  name: { type: String, required: true },
-  images: { type: Array, required: true },
-  location: {
-    coordinate: { type: Array, required: true },
-    type: { type: String, enum: exhibitionGeoTypeValues }
-  },
-  items: [itemSchema]
+  items: [itemSchema],
 });
 
-const exhibitionModel = model('Exhibition', exhibitionSchema);
+const exhibitionModel = model<ExhibitionIdentity>(
+  "Exhibition",
+  exhibitionSchema
+);
 
 export { exhibitionSchema, exhibitionModel };
