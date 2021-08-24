0b7fb6bfbb9c7bf76a14335177073eff6f7c830b
electrochemistry04 <electrochemistry04@outlook.com>
2021-08-24 20:04:18 +0900

    account interface

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,8 +1,13 @@
-import { UserType } from '../types';
+import { UserType } from "../types";
 
 export interface User {
-    idx: string,
-    username: string,
-    name: string,
-    userType: UserType
+  idx: string;
+  username: string;
+  name: string;
+  userType: UserType;
+}
+
+export interface Account {
+  idx: string;
+  password: string;
 }
