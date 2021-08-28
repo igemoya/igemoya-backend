f96ca8bb1d72594c1e809bbbe00f0c1ab3524db3
electrochemistry04 <electrochemistry04@outlook.com>
2021-08-29 00:59:52 +0900

    http status

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,38 @@ export const exhibitionGeoTypeValues = ["Point", "Polygon"] as const;
 export type exhibitionGeoType = typeof exhibitionGeoTypeValues[number];
 
 export type coordinate = [number, number];
+
+export enum HttpStatus {
+  Continue = 100,
+  SwitchingProtocol,
+  Processing,
+  EarlyHints,
+  OK = 200,
+  Created,
+  Accepted,
+  NonAuthInfo,
+  NoContent,
+  ResetContent,
+  PartialContent,
+  MultipleChoice = 300,
+  MovedPermanently,
+  Found,
+  SeeOther,
+  NotModified,
+  TempRedirect = 307,
+  PermRedirect,
+  BadRequest = 400,
+  Unauthorized,
+  Forbidden = 403,
+  NotFound,
+  MethodNotAllowed,
+  NotAcceptable,
+  RequestTimeout = 408,
+  Conflict,
+  Gone,
+  InternalServerError = 500,
+  NotImplemented,
+  BadGateway,
+  ServiceUnavailable,
+  GatewayTimeout,
+}
