6450723d3829540fcaac0885bb3f98d5c40eb45a
electrochemistry04 <electrochemistry04@outlook.com>
2021-08-31 22:28:23 +0900

    Exhibition CRUD

--- src/services/exhibition-supervising/controllers.ts
+++ src/services/exhibition-supervising/controllers.ts
@@ -5,11 +5,24 @@ import {
   ExhibitionIdentity,
   ItemIdentity,
   ObjectIdentity,
+  User,
 } from "../../interfaces";
 import { logger } from "../../resources/logger";
 import { ObjectId } from "mongodb";
 import { HttpStatus } from "../../types";
 import { Http } from "winston/lib/winston/transports";
+
+export const myExhibitions = async (req: Request, res: Response) => {
+  try {
+    const exhibitions: ExhibitionIdentity[] = await exhibitionModel.find({
+      createdUser: req.user._id,
+    });
+    return res.json({ exhibitions: exhibitions });
+  } catch (e) {
+    if (e.name === "HttpException") throw e;
+    throw new HttpException(HttpStatus.BadRequest, "전시 조회에 실패했습니다.");
+  }
+};
 
 export const registerExhibition = async (req: Request, res: Response) => {
   try {
@@ -20,22 +33,64 @@ export const registerExhibition = async (req: Request, res: Response) => {
       exhibition.location.coordinate[0],
     ];
     const newExhibition = new exhibitionModel(exhibition);
-    res.json(await newExhibition.save());
+    await newExhibition.save();
+    return res.sendStatus(HttpStatus.Created);
   } catch (e) {
     if (e.name === "HttpException") throw e;
     throw new HttpException(HttpStatus.BadRequest, "전시 등록에 실패했습니다.");
   }
 };
 
-export const myExhibitions = async (req: Request, res: Response) => {
+export const getExhibition = async (req: Request, res: Response) => {
   try {
-    const exhibitions: ExhibitionIdentity[] = await exhibitionModel.find({
-      createdUser: req.user._id,
-    });
-    res.json({ exhibitions: exhibitions });
+    const exhibition = await exhibitionModel.findById(req.params.id);
+    return res.json({ exhibition: exhibition });
   } catch (e) {
     if (e.name === "HttpException") throw e;
     throw new HttpException(HttpStatus.BadRequest, "전시 조회에 실패했습니다.");
+  }
+};
+
+export const updateExhibition = async (req: Request, res: Response) => {
+  try {
+    const exhibition = await exhibitionModel.findOne({
+      _id: req.body.exhibitionId,
+    });
+    if (exhibition.createdUser != req.user._id) {
+      //전시를 등록한 사용자와 현재 접근하는 사용자가 같은지 검증
+      throw new HttpException(
+        HttpStatus.Unauthorized,
+        "해당 전시를 수정하기 위해 필요한 권한이 없습니다."
+      );
+    }
+    await exhibitionModel.findByIdAndUpdate(
+      req.body.exhibitionId,
+      req.body.exhibition
+    );
+    res.sendStatus(HttpStatus.OK);
+  } catch (e) {
+    if (e.name === "HttpException") throw e;
+    throw new HttpException(HttpStatus.BadRequest, "전시 수정에 실패했습니다.");
+  }
+};
+
+export const deleteExhibition = async (req: Request, res: Response) => {
+  try {
+    const exhibition = await exhibitionModel.findOne({
+      _id: req.body.exhibitionId,
+    });
+    if (exhibition.createdUser != req.user._id) {
+      //전시를 등록한 사용자와 현재 접근하는 사용자가 같은지 검증
+      throw new HttpException(
+        HttpStatus.Unauthorized,
+        "해당 전시를 수정하기 위해 필요한 권한이 없습니다."
+      );
+    }
+    await exhibitionModel.findByIdAndDelete(req.body.exhibitionId);
+    res.sendStatus(HttpStatus.OK);
+  } catch (e) {
+    if (e.name === "HttpException") throw e;
+    throw new HttpException(HttpStatus.BadRequest, "전시 삭제에 실패했습니다.");
   }
 };
 
