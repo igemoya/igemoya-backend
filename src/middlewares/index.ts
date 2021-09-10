52086c84e63ccdeced19fc28ac3107c885b2c925
electrochemistry04 <electrochemistry04@outlook.com>
2021-09-10 12:30:52 +0900

    middleware 추가

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { default as attachIdentity } from "./user-identity";
 export { default as checkPermissions } from "./check-permissions";
+export { default as recombineCoord } from "./recombine-coord";
 export { default as validator } from "./validator";
